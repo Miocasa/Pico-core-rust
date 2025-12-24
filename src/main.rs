cea26152b8cc81f24662c2e043b1ce020cc4b4f6
Miocasa <narimanabdualiev06@gmail.com>
2025-12-24 23:30:42 +0500

    access for multiple device to i2c fixed. (Now full work two display on 1 i2c)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![no_std]
 #![no_main]
 
+use core::cell::RefCell;
+use cortex_m::interrupt::Mutex;
 use defmt_rtt as _;
 use embedded_graphics::{
     mono_font::{ascii::FONT_10X20, MonoTextStyle},
@@ -8,16 +10,63 @@ use embedded_graphics::{
     prelude::*,
     text::Text,
 };
-use embedded_graphics::text::{TextStyle, TextStyleBuilder};
 use panic_probe as _;
-use rp_pico::hal::{clocks::init_clocks_and_plls, gpio::Pins, i2c::I2C, pac, watchdog::Watchdog, Clock, Sio};
+use rp_pico::hal::{
+    clocks::init_clocks_and_plls,
+    gpio::Pins,
+    i2c::I2C,
+    pac,
+    watchdog::Watchdog,
+    Clock,
+    Sio
+};
 use rp_pico::hal::fugit::RateExtU32;
 use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
+
+type I2cType = I2C<
+    pac::I2C1,
+    (
+        rp_pico::hal::gpio::Pin<
+            rp_pico::hal::gpio::bank0::Gpio2,
+            rp_pico::hal::gpio::FunctionI2c,
+            rp_pico::hal::gpio::PullUp,
+        >,
+        rp_pico::hal::gpio::Pin<
+            rp_pico::hal::gpio::bank0::Gpio3,
+            rp_pico::hal::gpio::FunctionI2c,
+            rp_pico::hal::gpio::PullUp,
+        >,
+    ),
+>;
+
+static I2C_BUS: Mutex<RefCell<Option<I2cType>>> = Mutex::new(RefCell::new(None));
+
+// Обертка для разделяемого доступа к I2C
+struct SharedI2c;
+
+impl embedded_hal::i2c::ErrorType for SharedI2c {
+    type Error = rp_pico::hal::i2c::Error;
+}
+
+impl embedded_hal::i2c::I2c for SharedI2c {
+    fn transaction(
+        &mut self,
+        address: u8,
+        operations: &mut [embedded_hal::i2c::Operation<'_>],
+    ) -> Result<(), Self::Error> {
+        cortex_m::interrupt::free(|cs| {
+            if let Some(ref mut i2c) = I2C_BUS.borrow(cs).borrow_mut().as_mut() {
+                i2c.transaction(address, operations)
+            } else {
+                Err(rp_pico::hal::i2c::Error::Abort(0))
+            }
+        })
+    }
+}
 
 #[rp_pico::entry]
 fn main() -> ! {
     let mut pac = pac::Peripherals::take().unwrap();
-    // Core peripherals are not needed here; remove or underscore if retained elsewhere
     let _core = pac::CorePeripherals::take().unwrap();
 
     let mut watchdog = Watchdog::new(pac.WATCHDOG);
@@ -41,33 +90,61 @@ fn main() -> ! {
         &mut pac.RESETS,
     );
 
-    // I2C on GPIO4 (SDA) and GPIO5 (SCL)
+    // Создаем I2C шину на GPIO2 (SDA) и GPIO3 (SCL)
     let i2c = I2C::i2c1(
         pac.I2C1,
-        pins.gpio2.reconfigure(), // SDA 0 2
-        pins.gpio3.reconfigure(), // SCL 1 3
+        pins.gpio2.reconfigure(), // SDA
+        pins.gpio3.reconfigure(), // SCL
         400_u32.kHz(),
         &mut pac.RESETS,
         clocks.system_clock.freq(),
     );
 
-    let interface = I2CDisplayInterface::new_custom_address(i2c, 0x3D);
-    let mut display = Ssd1306::new(
-        interface,
+    // Помещаем шину в глобальный Mutex
+    cortex_m::interrupt::free(|cs| {
+        I2C_BUS.borrow(cs).replace(Some(i2c));
+    });
+
+    // Первый дисплей (адрес 0x3D)
+    let interface1 = I2CDisplayInterface::new_custom_address(SharedI2c, 0x3D);
+    let mut display1 = Ssd1306::new(
+        interface1,
         DisplaySize128x64,
         DisplayRotation::Rotate0,
     )
         .into_buffered_graphics_mode();
 
-    display.init().unwrap();
-    // let style = MonoTextStyle::new(&FONT_6X10, BinaryColor::On,);
-    let style = MonoTextStyle::new(&FONT_10X20, BinaryColor::On,);
-    // let style1 = TextStyleBuilder::new();
-    Text::new("RMK Display OK", Point::new(0, 10), style)
-        .draw(&mut display)
+    display1.init().unwrap();
+
+    let style = MonoTextStyle::new(&FONT_10X20, BinaryColor::On);
+    Text::new("RMK Display 1", Point::new(0, 20), style)
+        .draw(&mut display1)
+        .unwrap();
+    Text::new("OK", Point::new(0, 35), style)
+        .draw(&mut display1)
         .unwrap();
 
-    display.flush().unwrap();
+    display1.flush().unwrap();
+
+    // Второй дисплей (адрес 0x3C)
+    let interface2 = I2CDisplayInterface::new_custom_address(SharedI2c, 0x3C);
+    let mut display2 = Ssd1306::new(
+        interface2,
+        DisplaySize128x64,
+        DisplayRotation::Rotate0,
+    )
+        .into_buffered_graphics_mode();
+
+    display2.init().unwrap();
+
+    Text::new("RMK Display 2", Point::new(0, 20), style)
+        .draw(&mut display2)
+        .unwrap();
+    Text::new("OK", Point::new(0, 35), style)
+        .draw(&mut display2)
+        .unwrap();
+
+    display2.flush().unwrap();
 
     loop {
         cortex_m::asm::wfi();
